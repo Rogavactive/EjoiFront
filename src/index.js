2fe8720fc1eb5a29e316ee925d66f987414e59b3
Rogavactive <droga16@freeuni.edu.ge>
2020-03-07 15:07:39 +0400

    merge fix

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Gallery from './components/gallery/Gallery'
 
-<<<<<<< HEAD
-ReactDOM.render(<Gallery />, document.getElementById('root'))
-=======
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App";
 
@@ -13,4 +10,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
->>>>>>> 205a20f893def73c05e1df1674ff28f47f562c27
